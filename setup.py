4e86732e59b7bd918acd67266c333d9ae396f6d4
Bedapudi Praneeth <praneeth@bpraneeth.com>
2020-04-01 19:32:32 +0200

    update version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/bedapudi6788/Deep-Segmentation'
 EMAIL = 'praneeth@bpraneeth.com'
 AUTHOR = 'BEDAPUDI PRANEETH'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '2.3.0'
+VERSION = '2.3.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
